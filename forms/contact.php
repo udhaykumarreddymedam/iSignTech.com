c3019408fb460f7c00184d70adb48e26cb7661e4
udhaykumarreddymedam <udhaykumar.medam@isigntech.com>
2024-07-15 10:38:10 +0530

    Default Changes

---
+++ forms/contact.php
@@ -0,0 +1,35 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$receiving_email_address = 'info@isigntech.com';
+
+  $contact = new PHPMailer;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+ 
+  $contact->smtp = array(
+    'host' => 'smtp.hostinger.com',
+    'username' => 'example',
+    'password' => 'Welcome@2023',
+    'port' => '587'
+  );
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
