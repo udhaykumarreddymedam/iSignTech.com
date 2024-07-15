7c505839f83a9d7dff918f72fa3b2f3cc95844df
udhaykumarreddymedam <udhaykumar.medam@isigntech.com>
2024-07-15 11:07:30 +0530

    all previous changes updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,5 +263,17 @@
       clickable: true
     }
   });
+  
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1200,
+      // easing: 'ease-in-out',
+       once: true,
+      // mirror: false
+    })
+  });
 
 })()
